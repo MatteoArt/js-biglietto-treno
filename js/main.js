07fa5a9efb3fff4b0e8a190fae8c7092d986da85
MatteoArt <mattrim8@gmail.com>
2023-06-30 17:23:21 +0200

    completo programma(manca grafica)

--- js/main.js
+++ js/main.js
@@ -3,4 +3,24 @@ const age = prompt("Inserisci la tua età");
 
 //calcolo il prezzo totale in base al n di km
 const prezzo = kilometres * 0.21;
-console.log(prezzo);
+//console.log(prezzo);
+
+//il prezzo effettivo finale
+let prezzoFinale;
+let sconto;
+let coupon;
+
+if (age < 18) {
+    sconto = 20;
+    coupon = (prezzo * sconto) / 100; //quantità da sottrarre
+    prezzoFinale = prezzo - coupon;
+} else if (age > 65) {
+    sconto = 40;
+    coupon = (prezzo * sconto) / 100;
+    prezzoFinale = prezzo - coupon;
+} else {
+    prezzoFinale = prezzo;
+}
+ 
+
+console.log(prezzoFinale.toFixed(2));
