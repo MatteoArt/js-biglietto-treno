13a3fb5a124a1833354cd0c199eaa8f86fd8797b
MatteoArt <mattrim8@gmail.com>
2023-06-30 16:37:18 +0200

    inizio esercizio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const kilometres = prompt("Quanti kilometri vuoi percorrere?");
+const age = prompt("Inserisci la tua età");
+
+//calcolo il prezzo totale in base al n di km
+const prezzo = kilometres * 0.21;
+console.log(prezzo);
